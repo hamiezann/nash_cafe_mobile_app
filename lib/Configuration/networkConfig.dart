266ba78f04f0fa3cdb812cbcd46b56aa15b54a60
Nuraisyah <nuraisyahk1@gmail.com>
2024-07-09 09:37:47 +0800

    my

--- lib/Configuration/networkConfig.dart
+++ lib/Configuration/networkConfig.dart
@@ -1,6 +1,6 @@
 
 class Config {
-  // static const String apiUrl = 'http://192.168.43.243:80/api';
-  static const String apiUrl = 'http://192.168.35.29:80/api';
+
+  static const String apiUrl = 'http://172.20.10.8:80/api';
 
 }
